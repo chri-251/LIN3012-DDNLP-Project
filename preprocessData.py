ed7d9dc224923285b843ad9aabd731c1a4b77a05
tm-26 <sammut026@gmail.com>
2021-02-10 15:44:47 +0100

    Pre-processing done easy clap

--- preprocessData.py
+++ preprocessData.py
@@ -1,32 +1,71 @@
 import json
 
-if __name__ == "__main__":
 
+def preprocessData(languageAbbreviation, path, savePath):
     # Variable Declaration
     text = []
     labels = []
     noLabelsCount = 0
+    if languageAbbreviation == "us":
+        dictOfChars = {'❤': 0, '😍': 1, '😂': 2, '💕': 3, '🔥': 4, '😊': 5, '😎': 6, '✨': 7, '💙': 8, '😘': 9, '📷': 10, '🇺🇸': 11, '☀': 12, '💜': 13, '😉': 14, '💯': 15, '😁': 16, '🎄': 17, '📸': 18, '😜': 19}
+    else:
+        dictOfChars = {'❤': 0, '😍': 1, '😂': 2, '💕': 3, '😊': 4, '😘': 5, '💪': 6, '😉': 7, '👌': 8, '🇪🇸': 9, '😎': 10,
+                       '💙': 11, '💜': 12, '😜': 13, '💞': 14, '✨': 15, '🎶': 16, '💘': 17, '😁': 18}
 
-    with open("dataset/us/train/tweet_by_ID_08_2_2021__11_57_05.txt", encoding='utf-8') as file:
+    with open(path, encoding="utf-8") as file:
         tweets = file.read().split("\n")
         for i in range(len(tweets)):
             currentText = ''
             currentLabel = ''
             for character in json.loads(tweets[i])["text"]:
-                if character in ['❤', '😍', '😂', '💕', '🔥', '😊', '😎', '✨', '💙', '😘', '📷', '🇺🇸', '☀', '💜', '😉', '💯', '😁', '🎄', '📸', '😜']:
+                if character in ['❤', '😍', '😂', '💕', '🔥', '😊', '😎', '✨', '💙', '😘', '📷', '🇺🇸', '☀', '💜',
+                                 '😉', '💯', '😁', '🎄', '📸', '😜']:
                     if currentLabel == '':
                         currentLabel = character
-                        labels.append(character)
+                        labels.append(dictOfChars[character])
                     if currentLabel != character:
                         print("Error: Different labels detected in tweet #" + str(i))
                         exit(-1)
                 else:
-                    currentText += character
+                    # \n check
+                    if character == '\n':
+                        currentText += " "
+                    else:
+                        currentText += character
             if currentLabel == '':
                 noLabelsCount += 1
             else:
                 text.append(currentText)
-    print(noLabelsCount)
-    print(len(text))
-    print(len(labels))
 
+    # Clean data
+    for i in range(len(text)):
+        # Remove any spaces from the start of string
+        text[i].strip()
+
+        # Remove last link
+        if text[i].split()[-1].startswith("https://t.co/"):
+            text[i] = text[i].rsplit(' ', 1)[0]
+
+    if len(text) != len(labels):
+        print("Error: Number of tweets not equal to number of number of labels")
+        exit(-2)
+
+    print(str(noLabelsCount) + " tweets found without a label")
+    print(str(len(labels)) + " tweets found with a label")
+
+    with open(savePath + ".TEXT", 'w', encoding="utf-8") as f:
+        for i in text:
+            f.write("%s\n" % i)
+    print("TEXT file successfully generated")
+
+    with open(savePath + ".LABELS", 'w') as f:
+        for label in labels:
+            f.write("%s\n" % label)
+    print("LABELS file successfully generated")
+
+
+if __name__ == "__main__":
+    print("-------- Pre-processing us train data --------")
+    preprocessData("us", "dataset/us/train/trainOriginal.txt", "dataset/us/train/us_train")
+    print("-------- Pre-processing es train data --------")
+    preprocessData("es", "dataset/es/train/trainOriginal.txt", "dataset/es/train/es_train")
