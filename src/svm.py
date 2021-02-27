8659a5ed3537c5017fdc739cf4a76e78ac213f28
chri_251 <chribarbara@gmail.com>
2021-02-27 19:58:07 +0100

    Update svm.py
    Changed SVM used so it handles sparse matrices,  created function for simple preprocessing

--- src/svm.py
+++ src/svm.py
@@ -1,10 +1,5 @@
-# from nltk import ngrams
-# import nltk
-# from nltk.corpus import stopwords
-# import math
-# import pandas as pd
-# import numpy as np
-# import string
+from nltk.corpus import stopwords
+import string
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import svm
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
@@ -17,29 +12,54 @@ from spacy.lang.en import English
 from datetime import datetime
 
 
-def read_labels(train_filename, valid_filename, test_filename):
-    train_labels_file = open(train_filename, 'r')
-    valid_labels_file = open(valid_filename, 'r')
-    train_labels = train_labels_file.read().split("\n")[:-1] + valid_labels_file.read().split("\n")[:-1]
-    train_labels_file.close()
+def read_labels(labels_filename):
+    set_labels_file = open(labels_filename, 'r', encoding='utf-8')
+    set_labels = set_labels_file.read().split("\n")
+    set_labels_file.close()
 
-    test_labels_file = open(test_filename, 'r')
-    test_labels = test_labels_file.read().split("\n")[:-1]
-    test_labels_file.close()
+    if set_labels[len(set_labels) - 1] == '':
+        set_labels = set_labels[:-1]
 
-    return train_labels, test_labels
+    return set_labels
 
 
-# def get_classification():
-#     mappings = open("../dataset/us/raw data/us_mapping.txt", 'r', encoding="utf-8")
-#
-#     labels = []
-#     for line in mappings:
-#         split_line = line.split('\t')
-#         labels.append(split_line[1] + " " + split_line[2] + "(" + split_line[0] + ")")
-#
-#     mappings.close()
-#     return labels
+def simple_preprocessing(tweet_filename):
+    set_tweets = []
+
+    set_txt = open(tweet_filename, 'r', encoding='utf-8')
+
+    for line in set_txt:
+        prep_tweet = ''
+        lowercase_line = line.lower()
+        lowercase_line = lowercase_line.rstrip("\n")
+
+        # print(line,)  # The comma to suppress the extra new line char
+        lowercase_no_punc = lowercase_line.translate(str.maketrans('', '', string.punctuation))
+        # removes # and @ as well
+
+        for word in lowercase_no_punc.split():
+            if len(word) >= 2 and word not in stopWords:
+                prep_tweet += word + " "
+
+        prep_tweet = prep_tweet.strip()  # removes extra spaces at the front or back of prep_tweet
+
+        set_tweets.append(prep_tweet)
+
+    set_txt.close()
+
+    return set_tweets
+
+
+def get_preprocessed_tweets(tweets_filename):
+    set_tweets = []
+    set_tweets_file = open(tweets_filename, 'r', encoding='utf-8')
+
+    for line in set_tweets_file:
+        set_tweets.append(line)
+
+    set_tweets_file.close()
+
+    return set_tweets
 
 
 def spacy_tokenize(tweet):
@@ -48,36 +68,11 @@ def spacy_tokenize(tweet):
     return tokens
 
 
-def svm_tfidf(save, train_tweets, test_tweets, train_emoji_labels, test_emoji_labels):
-    # converts training and test data to TFIDF
-    vectorizer = TfidfVectorizer()
-    vectorizer.fit(train_tweets)
-    train_x_tfidf = vectorizer.transform(train_tweets)
-    test_x_tfidf = vectorizer.transform(test_tweets)
-    # print(vectorizer.vocabulary_)
-
-    if save:
-        print("Creating and fitting SVM classifier...")
-        svc = svm.SVC(C=1.0, kernel='linear', degree=3)
-        svc.fit(train_x_tfidf, train_emoji_labels)
-        pickle.dump(svc, open('../models/svc.sav', 'wb'))
-    else:
-        print("Loading existing SVM classifier...")
-        svc = pickle.load(open('../models/svc.sav', 'rb'))
-
-    # predict the labels for test set
-    pred_emoji_labels = svc.predict(test_x_tfidf)
-    # emoji_labels = get_classification()
-
-    print("\nAccuracy Score:", accuracy_score(test_emoji_labels, pred_emoji_labels))
-    print('\nConfusion Matrix:\n', confusion_matrix(test_emoji_labels, pred_emoji_labels))
-    print('\nClassification Report:\n', classification_report(test_emoji_labels, pred_emoji_labels))
-
-
-def svm_tfidf_pos(save, train_tweets, test_tweets, train_emoji_labels, test_emoji_labels):
+def svm_tfidf_pos_bigram(save, train_tweets, test_tweets, train_emoji_labels, test_emoji_labels):
     # train_tweets = train_tweets[:round((1/40)*len(train_tweets))]
     # train_emoji_labels = train_emoji_labels[:round((1 / 40) * len(train_emoji_labels))]
 
+    print("\nFitting TF-IDF Vectorizer and transforming data...")
     # converts training and test data to TFIDF
     vectorizer = TfidfVectorizer(tokenizer=spacy_tokenize, ngram_range=(1, 2))
     vectorizer.fit(train_tweets)
@@ -86,20 +81,20 @@ def svm_tfidf_pos(save, train_tweets, test_tweets, train_emoji_labels, test_emoj
     # print(vectorizer.vocabulary_)
 
     if save:
-        print("Creating and fitting SVM classifier...")
-        svc = svm.SVC(C=1.0, kernel='linear', degree=3)
+        print("\nCreating and fitting SVM classifier...")
+        # svc = svm.SVC(C=1.0, kernel='linear', degree=3)
+        svc = svm.LinearSVC()
         svc.fit(train_x_tfidf, train_emoji_labels)
-        pickle.dump(svc, open('../models/svc.sav', 'wb'))
+        pickle.dump(svc, open('../models/svc_preprocessed_us.sav', 'wb'))
     else:
-        print("Loading existing SVM classifier...")
+        print("\nLoading existing SVM classifier...")
         svc = pickle.load(open('../models/svc.sav', 'rb'))
 
     # predict the labels for test set
     pred_emoji_labels = svc.predict(test_x_tfidf)
-    # emoji_labels = get_classification()
 
     acc = accuracy_score(test_emoji_labels, pred_emoji_labels)
-    results =  classification_report(test_emoji_labels, pred_emoji_labels)
+    results = classification_report(test_emoji_labels, pred_emoji_labels)
 
     print("\nAccuracy Score:", acc)
     print('\nConfusion Matrix:\n', confusion_matrix(test_emoji_labels, pred_emoji_labels))
@@ -113,34 +108,27 @@ if __name__ == '__main__':
     # nltk.download()
 
     parser = English()
+    stopWords = stopwords.words("english")
 
-    train_file = "../dataset/us/pre-processed data/train/text.txt"
-    valid_file = "../dataset/us/pre-processed data/valid/text.txt"
-    test_file = "../dataset/us/pre-processed data/test/text.txt"
-    train_label_file = "../dataset/us/raw data/train/us_train.LABELS"
-    valid_label_file = "../dataset/us/raw data/valid/us_valid.labels"
-    test_label_file = "../dataset/us/raw data/test/us_test.LABELS"
+    simple = False
 
-    train_emoji_labels, test_emoji_labels = read_labels(train_label_file, valid_label_file, test_label_file)
+    if simple:
+        print("\nPerforming simple pre-processing...")
+        train_tweets = simple_preprocessing('../dataset/us/raw data/train/us_train.text')
+        train_tweets.extend(simple_preprocessing('../dataset/us/raw data/valid/us_valid.text'))
+        test_tweets = simple_preprocessing('../dataset/us/raw data/test/us_test.text')
+    else:
+        print("\nReading data pre-processed with token swapping...")
+        train_tweets = get_preprocessed_tweets('../dataset/us/pre-processed data/train/text.txt')
+        train_tweets.extend(get_preprocessed_tweets('../dataset/us/pre-processed data/valid/text.txt'))
+        test_tweets = get_preprocessed_tweets('../dataset/us/pre-processed data/test/text.txt')
 
-    # stores all train data tweets
-    train_tweets = []
-    with open(train_file, 'r', encoding='utf-8') as file:
-        for line in file:
-            train_tweets.append(line)
-    # since we are not using cross-validation in the current imp
-    with open(valid_file, 'r', encoding='utf-8') as file:
-        for line in file:
-            train_tweets.append(line)
+    train_emoji_labels = read_labels('../dataset/us/raw data/train/us_train.labels')
+    train_emoji_labels.extend(read_labels('../dataset/us/raw data/valid/us_valid.labels'))
+    test_emoji_labels = read_labels('../dataset/us/raw data/test/us_test.labels')
 
-    # stores all test data tweets
-    test_tweets = []
-    with open(test_file, 'r', encoding='utf-8') as file:
-        for line in file:
-            test_tweets.append(line)
+    svm_tfidf_pos_bigram(True, train_tweets, test_tweets, train_emoji_labels, test_emoji_labels)
 
-    svm_tfidf_pos(True, train_tweets, test_tweets, train_emoji_labels, test_emoji_labels)
-
-    # winsound.Beep(1500, 500)
+    winsound.Beep(1500, 500)
 
 # definitely: change code to suit US and ES
